c2b7e6dd95ede05952cea8203de449cabf6ee461
wangsiyu12344 <wangsiyu12344@163.com>
2020-06-20 16:41:31 +0900

    .

--- src/javadesignmode/Singleton5.java
+++ src/javadesignmode/Singleton5.java
@@ -1,8 +1,25 @@
 package javadesignmode;
 
 /**
- * 测试
+ * 双重检查:推荐在面试的时候使用。 是线程安全的.
+ * 为什么要用volatile关键字? 因为新建对象不是原子操作。 为了防止指令的重排序。
+ * 新建一个对象其实有3个步骤 ： 1.分配内存空间； 2.执行构造方法来初始化字段的值； 3.把instance对象指向分配的内存空间。
+ * 如果是执行顺序是1->3->2（发生了指令的重排序）， 那么最后得到的instance不是我们期望的值。
  */
 public class Singleton5 {
+    private volatile static Singleton5 instance;
 
+    private Singleton5(){}
+
+    public Singleton5 getInstance(){
+        if(instance == null){
+            synchronized (Singleton5.class){
+                if(instance == null){
+                    instance = new Singleton5();
+                }
+
+            }
+        }
+        return instance;
+    }
 }
